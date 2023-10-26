10c0504c843d45f8babae4bf59372ec16b4e6008
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-10-27 01:00:22 +0900

    feature: 상수 파일로 리스트 띄우기

--- Week1/Ledger/index.js
+++ Week1/Ledger/index.js
@@ -2,3 +2,10 @@ const INIT_BALANCE = 0;
 const deleteButton = document.querySelectorAll(".delete_button");
 
 deleteButton.addEventListener
+  const listDeleteButton = document.createElement("button");
+  listDeleteButton.type = "button";
+  listDeleteButton.innerText = "X";
+  listDeleteButton.addEventListener("click", () => {
+    listDeleteButton.parentNode.remove();
+  });
+});
