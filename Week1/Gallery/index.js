212b49fa84823d1e77f024498cbda21714c91c3b
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-10-26 18:10:52 +0900

    feature: 호버 시 텍스트 보이게 설정

--- Week1/Gallery/index.js
+++ Week1/Gallery/index.js
@@ -2,15 +2,28 @@ const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
 const pageHeight = window.innerHeight;
-const topButton = document.querySelector(".button-top");
-const pandaImages = document.querySelectorAll(".images-panda img");
-const imageDescriptions = document.querySelectorAll(".images-panda img + p");
+const topButton = $(".button-top");
+const pandaImages = $$(".images-panda img");
+const pandaCardWrappers = $$(".images-panda-wrapper");
 
-//투명도 조절
+//맨 위로 버튼 투명도 조절
 document.addEventListener("scroll", () => {
 	topButton.style.opacity = window.scrollY / pageHeight;
 });
 
-pandaImages.addEventListener("mouseover", () => {
-	imageDescriptions.style.display = "block";
+//호버 시 텍스트 보이게 설정
+pandaCardWrappers.forEach((pandaCardWrapper) => {
+	pandaCardWrapper.addEventListener("mouseover", () => {
+		const description = pandaCardWrapper.querySelector(
+			".images-panda__description"
+		);
+		description.classList.add("description-appear");
+	});
+
+	pandaCardWrapper.addEventListener("mouseout", () => {
+		const description = pandaCardWrapper.querySelector(
+			".images-panda__description"
+		);
+		description.classList.remove("description-appear");
+	});
 });
