70d29b62318319999eea108b88ae2bdfa8cb8ca9
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-10-27 01:01:18 +0900

    feature: 상수 파일로 리스트 띄우기

--- Week1/Ledger/index.js
+++ Week1/Ledger/index.js
@@ -1,5 +1,34 @@
+import { HISTORY_LIST } from "./constants/HISTORY.js";
+
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
 const INIT_BALANCE = 0;
 const deleteButton = document.querySelectorAll(".delete_button");
+const deleteButton = $$(".delete_button");
+const listContainer = $(".list-container");
+
+HISTORY_LIST.forEach((history, index) => {
+  const listItem = document.createElement("li");
+  const { category, subject, amount, type } = history;
+
+  const spanCategory = document.createElement("span");
+  spanCategory.className = "list__category";
+  spanCategory.textContent = category;
+  listItem.appendChild(spanCategory);
+
+  const spanSubject = document.createElement("span");
+  spanSubject.className = "subject";
+  spanSubject.textContent = subject;
+  listItem.appendChild(spanSubject);
+
+  const spanAmount = document.createElement("span");
+  spanAmount.className = "amount";
+  spanAmount.textContent = type === "수입" ? amount : -amount;
+  spanAmount.classList.add(type === "수입" ? "plus" : "minus");
+  listItem.appendChild(spanAmount);
+
+  listContainer.appendChild(listItem);
 
 deleteButton.addEventListener
   const listDeleteButton = document.createElement("button");
