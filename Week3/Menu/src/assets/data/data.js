8cb8cbcabcf6e6dc4b4e20895a03f67013ccff4e
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-10 16:36:48 +0900

    feature: created menu items data

---
+++ Week3/Menu/src/assets/data/data.js
@@ -0,0 +1,113 @@
+import bibimbap from "./images/bibimbap";
+import bulgogibap from "./images/bulgogibap";
+import butadon from "./images/butadong";
+import beef_noodle from "./images/beef_noodle";
+import chinese_pork_rice from "./images/chinese_pork_rice";
+import suan_la_fen from "./images/suan_la_fen";
+import eggplant_rice from "./images/eggplant_rice";
+import fish_noodle from "./images/fish_noodle";
+import yakisoba from "./images/yakisoba";
+import jjambbong from "./images/jjambbong";
+import kimchi_noodle from "./images/kimchi_noodle";
+import korean_meat_noodle from "./images/korean_meat_noodle";
+import nikku_udon from "./images/nikku_udon";
+import onigiri from "./images/onigiri";
+import shrimp_rice from "./images/shrimp_rice";
+import soba from "./images/soba";
+import crab from "./images/crab";
+import sushi from "./images/sushi";
+
+const MENU_ITEMS = [
+  {
+    name: "비빔밥",
+    category: ["korean", "vegan", "rice"],
+    img: bibimbap,
+  },
+  {
+    name: "불고기 덮밥",
+    category: ["korean", "meat", "rice"],
+    img: bulgogibap,
+  },
+  {
+    name: "부타동",
+    category: ["japanese", "meat", "rice"],
+    img: butadon,
+  },
+  {
+    name: "우육면",
+    category: ["chinese", "meat", "noodle"],
+    img: beef_noodle,
+  },
+  {
+    name: "중국식 돼지고기 덮밥",
+    category: ["chinese", "meat", "rice"],
+    img: chinese_pork_rice,
+  },
+  {
+    name: "쏸라펀",
+    category: ["chinese", "vegan", "noodle"],
+    img: suan_la_fen,
+  },
+  {
+    name: "어향가지덮밥",
+    category: ["chinese", "vegan", "rice"],
+    img: eggplant_rice,
+  },
+  {
+    name: "물회",
+    category: ["korean", "seafood", "noodle"],
+    img: fish_noodle,
+  },
+  {
+    name: "야끼소바",
+    category: ["japanese", "seafood", "noodle"],
+    img: yakisoba,
+  },
+  {
+    name: "짬뽕",
+    category: ["chinese", "seafood", "noodle"],
+    img: jjambbong,
+  },
+  {
+    name: "김치 잔치국수",
+    category: ["korean", "vegan", "noodle"],
+    img: kimchi_noodle,
+  },
+  {
+    name: "고기 국수",
+    category: ["korean", "meat", "noodle"],
+    img: korean_meat_noodle,
+  },
+  {
+    name: "니꾸우동",
+    category: ["japanese", "meat", "noodle"],
+    img: nikku_udon,
+  },
+  {
+    name: "오니기리",
+    category: ["japanese", "vegan", "rice"],
+    img: onigiri,
+  },
+  {
+    name: "새우볶음밥",
+    category: ["chinese", "seafood", "rice"],
+    img: shrimp_rice,
+  },
+  {
+    name: "소바",
+    category: ["japanses", "vegan", "noodle"],
+    img: soba,
+  },
+  {
+    name: "간장게장",
+    category: ["korean", "seafood", "rice"],
+    img: crab,
+  },
+  {
+    name: "스시",
+    category: ["japanese", "seafood", "rice"],
+    img: sushi,
+  },
+];
+
+export default MENU_ITEMS;
