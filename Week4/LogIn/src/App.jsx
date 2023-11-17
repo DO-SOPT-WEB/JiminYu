276a7bade7632e8dfb2d725d230e253b8ebf5428
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-17 18:48:19 +0900

    styles: created theme and GlobalStyle

--- Week4/LogIn/src/App.jsx
+++ Week4/LogIn/src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
+
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
+import GlobalStyle from "../styles/GlobalStyle";
+import Router from "../src/components/Router";
 
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ Week4/LogIn/styles/GlobalStyle.js
@@ -0,0 +1,49 @@
+import { createGlobalStyle, css } from "styled-components";
+
+const reset = css`
+  * {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+    color: inherit;
+  }
+  *,
+  :after,
+  :before {
+    box-sizing: border-box;
+  }
+  :root {
+    -webkit-tap-highlight-color: transparent;
+    -webkit-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    cursor: default;
+    line-height: 1.5;
+    overflow-wrap: break-word;
+    word-break: break-word;
+    tab-size: 4;
+
+    background-color: ${({ theme }) => theme.colors.gray};
+  }
+  html,
+  body {
+    height: 100%;
+  }
+  button {
+    background: none;
+    border: 0;
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
+
+export const GlobalStyle = createGlobalStyle`
+${reset} 
+`;
+
+export default GlobalStyle;
